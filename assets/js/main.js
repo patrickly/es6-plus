e5d43d05eff961641c8b0b7b46b47be71ff357b5
Patrick Ly <patrickkimly@gmail.com>
2019-09-18 15:57:11 -0700

    Array.flat()

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,12 @@
-const steps = [
-	new Promise(resolve => resolve('We wake up')),
-	new Promise(resolve => resolve('Eat breakfast')),
-	new Promise(resolve => resolve('Code'))
+// const names = [
+//     ['joe', 'billy'],
+//     ['cindy', 'jenny']
+// ];
+
+const names = [
+	['joe', 'billy'],
+	['cindy', 'jenny', [23, 4, 56, 7, [1, 2, 3, 4, [1, 2, 3, 4, [1, 2, 3, 4]]]]]
 ];
-
-async function runSteps() {
-	for await (const step of steps) {
-		console.log(step);
-	}
-}
-
-runSteps();
+console.log(names.flat());
+console.log(names.flat(3));
+console.log(names.flat(Infinity));
