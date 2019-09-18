2018713ba142aa68827a7414d23edf94b28bba90
Patrick Ly <patrickkimly@gmail.com>
2019-09-18 14:18:21 -0700

    rest properties for objects

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,56 +1,13 @@
-// async await
-
-const getUser = fname => {
-	var user = {
-		id: 6,
-		fname: 'Jane'
-	};
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			console.log('getting user');
-			if (fname == user.fname) {
-				resolve(user);
-			} else {
-				reject('user not found');
-			}
-		}, 2000);
-	});
+let user = {
+	fname: 'John',
+	lname: 'Doe',
+	age: 22,
+	phone: '123-456-7890'
 };
 
-const getTweet = id => {
-	var tweet = {
-		user_id: 6,
-		post: 'Love star wars'
-	};
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			console.log('getting tweet');
-			if (tweet.user_id === id) {
-				resolve(tweet);
-			} else {
-				reject('no tweets found for this user');
-			}
-		}, 2000);
-	});
-};
+// let { fname, lname } = user;
+// console.log(`${fname} ${lname}`);
 
-const printUserTweet = async fname => {
-	try {
-		const tweet = await getTweet(user.id);
-		const user = await getUser(fname);
-		console.log(`${user.fname} ${tweet.post}`);
-		return `${user.fname} ${tweet.post}`;
-	} catch (error) {
-		console.log(`Error: ${error}`);
-	}
-};
-
-// printUserTweet('Jane')
-// 	.then(value => {
-// 		console.log(value);
-// 	})
-// 	.catch(error => {
-// 		console.log(`Error: ${error}`);
-//     });
-
-printUserTweet('Jane');
+let { fname, lname, ...others } = user;
+console.log(`${fname} ${lname}`);
+console.log(others);
