af97b7dcf1f402f772acbc8f73c233d2b214e109
Patrick Ly <patrickkimly@gmail.com>
2019-09-18 11:59:31 -0700

    async await 6:08

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,44 @@
-/* const printUser = (name, age) => {
-	console.log(`${name} ${age}`);
-};
- */
-const printUser = (name, age,) => {
-	console.log(`${name} ${age}`);
+// async await
+
+const getUser = fname => {
+	var user = {
+		id: 3,
+		fname: 'Jane'
+	};
+	return new Promise((resolve, reject) => {
+		console.log('getting user');
+		setTimeout(() => {
+			if (fname == user.fname) {
+				resolve(user);
+			} else {
+				reject('Error: user not found');
+			}
+		}, 2000);
+	});
 };
 
-printUser('pat', 18);
+const getTweet = id => {
+	var tweet = {
+		user_id: 6,
+		post: 'Love star wars'
+	};
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			console.log('getting tweet');
+			if (tweet.user_id === id) {
+				resolve(tweet);
+			} else {
+				reject('Error: no tweets found for this user');
+			}
+		}, 2000);
+	});
+};
+
+const printUserTweet = async fname => {
+	const user = await getUser(fname);
+	const tweet = await getTweet(user.id);
+
+	return `${user.fname} ${tweet.post}`;
+};
+
+console.log(printUserTweet('Jane'));
